9d8a207ee6ada6cd3e1263cddba92fa08b1c7e13
beugen7171 <beugenx@gmail.com>
2015-11-04 01:01:36 +0000

    Done.

---
+++ first_program.rb
@@ -0,0 +1,26 @@
+# what is a program
+puts "this is a program"
+puts "program are interpreted at runtime"
+puts "which measn even though i am writing this tuesday november 3rd 1359 hrs. the time is now" + Time.now.to_s
+puts "programs have flow, like water flow, they run one string after the other"
+
+puts "which means you see this first"
+puts "and now you see this second"
+puts " and you see this third"
+
+puts "lines of code are executed linearly, thats its flow"
+puts "first come, first serve"
+
+puts "programs are composed of three things"
+puts "a LANGUAGES KEYWORDS, like 'if' or 'and', approx. 43"
+puts "like literal pieces of data, like this sentence / STRING."
+puts " finally BAREWORDS and VARIABLES, that are set equal to things"
+puts "anything that isnt one of those will cause and error"
+puts "like the word 'see' below, i removed it so the progrm rruns to the end"
+
+puts "'see' by itself is not a string, its not a language keyword, its not barewords or variable"
+puts "WORDS IN A PROGRAM
+Every word and character in a program has to be valid code for the ruby language. Basically, every word can be one of three possible things:
+1/A Ruby keyword, something that's part of the ruby language.
+2/Literal data, things like 'Strings' and Numbers 1,2.
+3/Barewords you define and create, things like variables and methods."
